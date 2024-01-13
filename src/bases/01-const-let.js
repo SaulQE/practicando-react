491f64810c8dbd49c69641c5a21846a3c9c0888d
SaulQE <saul.esquivel.028@gmail.com>
2024-01-13 15:35:49 -0500

    practica-react

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,19 @@
+
+// Variables y Constantes
+
+const nombre = 'Saul';
+const apellido = 'Esquivel'
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre,apellido,valorDado);
+
+// var no se debe usar
+if(true){
+    const nombre = 'peter';
+    console.log(nombre);
+}
+
+console.log(valorDado);
+
